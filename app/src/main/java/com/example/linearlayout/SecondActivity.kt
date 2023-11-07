146c78d8b5e17bc2bddb55e0f5838c5210853495
josego16 <jgomlin@g.educaand,es>
2023-11-07 17:28:57 +0100

    hemos creado un linearLayout con orientacion vertical y horizontal

--- app/src/main/java/com/example/linearlayout/SecondActivity.kt
+++ app/src/main/java/com/example/linearlayout/SecondActivity.kt
@@ -3,10 +3,9 @@ package com.example.linearlayout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_second)
     }
 }
